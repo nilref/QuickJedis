e2289f1ec1dae1d94bf591ba18b36fa75ca60de5
404 Not Found <mahatmasmile@qq.com>
2016-08-23 09:41:40 +0800

    Update cleanMavenLastUpdated.bat

--- cleanMavenLastUpdated.bat
+++ cleanMavenLastUpdated.bat
@@ -1,6 +1,6 @@
 @echo off
 set REPOSITORY_PATH=E:\maven\maven-dependcies
-rem 正在搜索...
+rem 正在搜索....
 for /f "delims=" %%i in ('dir /b /s "%REPOSITORY_PATH%\*lastUpdated*"') do (
     del /s /q %%i
 )
