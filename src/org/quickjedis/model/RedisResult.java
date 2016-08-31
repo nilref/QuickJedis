9fb64863d3e9c4e0bffdb03af5bd623f6aefe59d
mahatmasmile <mahatmasmile@qq.com>
2016-08-31 16:42:02 +0800

    Update

--- src/org/quickjedis/model/RedisResult.java
+++
@@ -1,7 +0,0 @@
-package org.quickjedis.model;
-
-public class RedisResult {
-
-	public static final String OK = "OK";
-	public static final Long SUCCESS = new Long(1);
-}
--- src/org/quickjedis/utils/StringHelper.java
+++
@@ -1,8 +0,0 @@
-package org.quickjedis.utils;
-
-public class StringHelper {
-
-	public static Boolean IsNullOrEmpty(String str) {
-		return null == str || "" == str;
-	}
-}
